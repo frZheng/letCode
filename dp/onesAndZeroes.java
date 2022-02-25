d75b802a8f72ead4cb608147914d6bb8d8f4d054
frZheng <fr_zheng@163.com>
2022-02-25 18:00:03 +0800

    commit

--- dp/onesAndZeroes.java
+++ dp/onesAndZeroes.java
@@ -1,28 +1,40 @@
 package dp;
 
 //web https://leetcode-cn.com/problems/unique-paths/
-public class uniquePaths {
+public class  onesAndZeroes {
     private static class Solution {
 
         public Solution() {
         }
 
-        public int uniquePaths(int m, int n) {
-            if (m == 0 || n==0) return 1;
-            int[][] dp = new int[m][n];
-            for (int i = 0; i < m; ++i) dp[i][0] = 1;
-            for (int i = 1; i < n; ++i) dp[0][i] = 1;
-            for (int i = 1; i < m; ++i) {
-                for (int j = 1; j < n; ++j) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        public int findMaxForm(String[] strs, int numOfZero, int numOfOne) {
+            if (strs.length == 0) return 0;
+            int[][] dp = new int[numOfZero + 1][numOfOne + 1];
+
+            for( String s : strs) {
+                int oneNum = 0, zeroNum = 0;
+                for (char c : s.toCharArray()) {
+                    if (c == '0') ++zeroNum;
+                    else ++oneNum;
                 }
+//                System.out.println(oneNum + " " + zeroNum);
+                for (int i = numOfZero; i >= zeroNum; --i) {
+                    for (int j = numOfOne; j >= oneNum; --j) {
+                        int row = i - zeroNum;
+                        int col = j - oneNum;
+//                        System.out.println("in: " + row + " " + col);
+                        dp[i][j] = Math.max(dp[i][j], dp[row][col] + 1);
+                    }
+                }
+
             }
-            return dp[m-1][n-1];
+            return dp[numOfZero][numOfOne];
         }
     }
     public static void main(String Args[]) {
         Solution solution = new Solution();
-        int fib = solution.uniquePaths(3,4);
+        String [] strs = {"10", "0001", "111001", "1", "0"};
+        int fib = solution.findMaxForm(strs, 5, 3);
         System.out.println(fib);
     }
 }
