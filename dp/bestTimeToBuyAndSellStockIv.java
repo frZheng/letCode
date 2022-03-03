2385af7b18b6ebc904446fc7581512892332d04e
frZheng <fr_zheng@163.com>
2022-03-03 18:18:07 +0800

    commit

--- dp/bestTimeToBuyAndSellStockIv.java
+++ dp/bestTimeToBuyAndSellStockIv.java
@@ -7,27 +7,26 @@ public class bestTimeToBuyAndSellStockIv {
         public Solution() {
         }
 
-        public int maxProfit(int[] prices) {
+        public int maxProfit(int k, int[] prices) {
             int len = prices.length;
-            if (len < 1) return 0;
-            int[] dp = new int[5];
-
-            dp[1] = -prices[0];
-            dp[3] = -prices[0];
-
-            for (int i = 1; i < len; ++i) {
-                dp[1] = Math.max(dp[1], dp[0] - prices[i]);
-                dp[2] = Math.max(dp[2], dp[1] + prices[i]);
-                dp[3] = Math.max(dp[3], dp[2] - prices[i]);
-                dp[4] = Math.max(dp[4], dp[3] + prices[i]);
+            if (len == 0) return 0;
+            int[][] dp = new int[len][k * 2 + 1];
+            for (int j = 1; j < 2 * k; j += 2) {
+                dp[0][j] = -prices[0];
             }
-            return dp[4];
+            for (int i = 1; i < len; ++i) {
+                for (int j = 0; j < 2 * k - 1; j += 2) {
+                    dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                    dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+                }
+            }
+            return dp[len - 1][2 * k];
         }
     }
     public static void main(String Args[]) {
         Solution solution = new Solution();
-        int [] cost = {7,1,5,3,6,4};
-        int fib = solution.maxProfit(cost);
+        int [] cost = {3,2,6,5,0,3};
+        int fib = solution.maxProfit(2, cost);
         System.out.println(fib);
     }
 }
