b6f097d6c195d6b159604a7a44e83b2e1a3792ad
frZheng <fr_zheng@163.com>
2022-03-03 23:45:03 +0800

    commit

---
+++ dp/bestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,20 @@
+package dp;
+
+//web https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown
+public class bestTimeToBuyAndSellStockWithCooldown {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public int maxProfit(int[] prices) {
+
+        }
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        int [] cost = {1,2,3,0,2};
+        int fib = solution.maxProfit(cost);
+        System.out.println(fib);
+    }
+}
