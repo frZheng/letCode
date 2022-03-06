edf12651de824ca5ccdfe0f05d8e13a89cc2e944
frZheng <fr_zheng@163.com>
2022-03-06 09:57:01 +0800

    commit

---
+++ dp/uncrossedLines.java
@@ -0,0 +1,32 @@
+package dp;
+
+//web https://leetcode-cn.com/problems/uncrossed-lines
+public class  uncrossedLines {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public int maxUncrossedLines(int[] nums1, int[] nums2) {
+            int [][] dp = new int[nums1.length+1][nums2.length+1];
+            for(int i=1;i<=nums1.length;i++) {
+                for(int j=1;j<=nums2.length;j++) {
+                    if (nums1[i-1]==nums2[j-1]) {
+                        dp[i][j]=dp[i-1][j-1]+1;
+                    }
+                    else {
+                        dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+            return dp[nums1.length][nums2.length];
+        }
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 4, 2};
+        int[] nums2 = {1, 2, 4};
+        int fib = solution. maxUncrossedLines(nums1, nums2);
+        System.out.println(fib);
+    }
+}
