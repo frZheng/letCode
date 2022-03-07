a41ceb68d6e1fcabc85c21b3adcc148d6b8643c0
frZheng <fr_zheng@163.com>
2022-03-07 11:05:37 +0800

    commit

---
+++ sql/classesMoreThan5Students.sql
@@ -0,0 +1,16 @@
+-- web: https://leetcode-cn.com/problems/classes-more-than-5-students
+drop table if EXISTS World;
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+-- 请列出所有超过或等于5名学生的课。
+
+select class from courses group by class having count(distinct student) >= 5
