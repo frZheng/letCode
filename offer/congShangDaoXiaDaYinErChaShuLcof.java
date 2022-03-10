e3d9b0e45ba1d6ed321b5903102ae0e9e15cacb9
frZheng <fr_zheng@163.com>
2022-03-10 23:50:00 +0800

    commit

---
+++ offer/congShangDaoXiaDaYinErChaShuLcof.java
@@ -0,0 +1,24 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+
+
+import Trees.Tree.TreeNode;
+
+public class congShangDaoXiaDaYinErChaShuLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+        public int[] levelOrder(TreeNode root) {
+        }
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        
+        char s = solution.levelOrder("");
+        System.out.println("res: " + s);
+
+    }
+		
+}
