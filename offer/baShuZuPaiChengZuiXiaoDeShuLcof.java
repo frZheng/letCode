454d1e965ae41bc60014480bb570af61ff331f42
frZheng <fr_zheng@163.com>
2022-03-14 23:45:03 +0800

    commit

---
+++ offer/baShuZuPaiChengZuiXiaoDeShuLcof.java
@@ -0,0 +1,23 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof
+
+public class baShuZuPaiChengZuiXiaoDeShuLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public String minNumber(int[] nums) {
+            return "";
+        }
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        int[] arr = {3,30,34,5,9,1};//30,3,1,34,5,9
+        String lists = solution.minNumber(arr);
+        System.out.println(lists);
+
+    }
+
+}
