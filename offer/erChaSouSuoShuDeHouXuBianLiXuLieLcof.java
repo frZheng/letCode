f311a096c9b04068131e41aae7a7bd2b1c7eed05
frZheng <fr_zheng@163.com>
2022-03-31 13:00:03 +0800

    commit

---
+++ offer/erChaSouSuoShuDeHouXuBianLiXuLieLcof.java
@@ -0,0 +1,56 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof
+
+
+
+public class erChaSouSuoShuDeHouXuBianLiXuLieLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+        // 递归实现
+        private boolean verify(int[] postorder, int left, int right){
+            if (left >= right) return true; // 当前区域不合法的时候直接返回true就好
+
+            int rootValue = postorder[right]; // 当前树的根节点的值
+
+            int k = left;
+            while (k < right && postorder[k] < rootValue){ // 从当前区域找到第一个大于根节点的，说明后续区域数值都在右子树中
+                k++;
+            }
+
+            for (int i = k; i < right; i++){ // 进行判断后续的区域是否所有的值都是大于当前的根节点，如果出现小于的值就直接返回false
+                if (postorder[i] < rootValue) return false;
+            }
+
+            // 当前树没问题就检查左右子树
+            if (!verify(postorder, left, k - 1)) return false; // 检查左子树
+
+            if (!verify(postorder, k, right - 1)) return false; // 检查右子树
+
+            return true; // 最终都没问题就返回true
+        }
+        public boolean verifyPostorder(int[] postorder) {
+            if (postorder.length < 2) return true;
+            return verify(postorder, 0, postorder.length - 1);
+        }
+    }
+
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+
+
+//        String str = "3,4,5,1,2,null,null";
+
+
+        int[] inodert = {1,6,3,2,5};
+
+        boolean b = solution.verifyPostorder(inodert);
+        System.out.println(b);
+
+    }
+
+
+
+}
--- offer/shuZhiDeZhengShuCiFangLcof.java
+++ offer/shuZhiDeZhengShuCiFangLcof.java
@@ -13,18 +13,12 @@ public class  shuZhiDeZhengShuCiFangLcof {
         public Solution() {
         }
         public double myPow(double x, int n) {
-            if (x == 0.0) return 0.0;
-            double sum = x;
-            int flag = 1;
-            if (n < 0) {
-                flag = 0;
-                n = -n;
-            }
-            for (int i = 1; i < n; ++i){
-                sum*=x;
-            }
-            if(flag == 0) sum = 1/sum;
-            return sum;
+            if (n == 0) return 1;
+            if (n == 1) return x;
+            if(n == -1) return 1 / x;
+            double half = myPow(x, n / 2);
+            double mod = myPow(x, n % 2);
+            return half * half * mod;
         }
     }
 
