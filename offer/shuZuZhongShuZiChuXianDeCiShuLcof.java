ef3b89df6d9f965b3ba6d8de2a8e4b0f67c8caf3
frZheng <fr_zheng@163.com>
2022-04-01 13:00:00 +0800

    commit

---
+++ offer/shuZuZhongShuZiChuXianDeCiShuLcof.java
@@ -0,0 +1,31 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof
+
+
+
+public class shuZuZhongShuZiChuXianDeCiShuLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+        public int[] singleNumbers(int[] nums) {
+        }
+    }
+
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+
+
+//        String str = "3,4,5,1,2,null,null";
+
+        int[] nums = {4,1,4,6};
+        int[] bs = solution.singleNumbers(nums);
+        for (int b : bs)
+            System.out.println(b);
+
+    }
+
+
+
+}
