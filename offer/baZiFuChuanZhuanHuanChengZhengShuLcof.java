9518232b832779ca12e4c2189015830c21a6fed5
frZheng <fr_zheng@163.com>
2022-04-06 18:00:00 +0800

    commit

---
+++ offer/baZiFuChuanZhuanHuanChengZhengShuLcof.java
@@ -0,0 +1,29 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class baZiFuChuanZhuanHuanChengZhengShuLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+
+        public int strToInt(String str) {
+            
+        }
+
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        String num = "42";
+        int lists = solution.strToInt(num);
+        System.out.println(lists);
+
+    }
+
+}
