1ce873478cea5701096299b41bbe212e38e0cd76
frZheng <fr_zheng@163.com>
2022-04-12 18:00:01 +0800

    commit

---
+++ offer/zhengZeBiaoDaShiPiPeiLcof.java
@@ -0,0 +1,28 @@
+package offer;
+
+//web https://leetcode-cn.com/problems/zheng-ze-biao-da-shi-pi-pei-lcof
+
+import java.util.*;
+
+public class zhengZeBiaoDaShiPiPeiLcof {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+
+        public boolean isMatch(String s, String p) {
+            
+        }
+
+    }
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+        String s = "abc";
+        String p = "abc";
+        boolean lists = solution.isMatch(s, p);
+        System.out.println(lists);
+
+    }
+
+}
