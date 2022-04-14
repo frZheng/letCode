d2c8d939ed16a57c4daa2ea8cfb8ab16b6a1bcb7
frZheng <fr_zheng@163.com>
2022-04-14 18:00:00 +0800

    commit

--- offer/zhengZeBiaoDaShiPiPeiLcof.java
+++ offer/zhengZeBiaoDaShiPiPeiLcof.java
@@ -12,8 +12,29 @@ public class zhengZeBiaoDaShiPiPeiLcof {
 
 
         public boolean isMatch(String s, String p) {
+            int m = s.length();
+            int n = p.length();
 
+            boolean[][] f = new boolean[m + 1][n + 1];
+            f[0][0] = true;
+            for (int i = 0; i <= m; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    if (p.charAt(j - 1) == '*') {
+                        f[i][j] = f[i][j - 2];
+                        if (matches(s, p, i, j - 1)) {
+                            f[i][j] = f[i][j] || f[i - 1][j];
+                        }
+                    } else {
+                        if (matches(s, p, i, j)) {
+                            f[i][j] = f[i - 1][j - 1];
+                        }
+                    }
+                }
+            }
+            return f[m][n];
         }
+
+
 
     }
     public static void main(String Args[]) {
