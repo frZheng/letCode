55703b037dd4409090cf366657e17298b1c9277c
frZheng <fr_zheng@163.com>
2022-04-27 18:00:00 +0800

    commit

---
+++ top100/zigzagConversion.java
@@ -0,0 +1,32 @@
+package top100;
+
+//web https://leetcode-cn.com/problems/zigzag-conversion
+
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class  zigzagConversion {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+
+        public String convert(String s, int numRows) {
+            
+        }
+    }
+
+
+    public static void main(String[] Args) {
+        Solution solution = new Solution();
+
+
+        String s = solution.convert("PAYPALISHIRING",3);
+        System.out.println(s);
+
+    }
+}
