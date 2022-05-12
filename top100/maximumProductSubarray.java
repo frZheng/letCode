320195123408fee45d70964b476c957c9049a284
frZheng <fr_zheng@163.com>
2022-05-12 18:00:00 +0800

    commit

---
+++ top100/maximumProductSubarray.java
@@ -0,0 +1,33 @@
+package top100;
+
+//web  https://leetcode.cn/problems/maximum-product-subarray
+
+
+import Trees.Tree.TreeNode;
+import list.ListNode;
+
+import java.util.*;
+
+import static Trees.Tree.TreeNode.*;
+
+public class  maximumProductSubarray {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public int maxProduct(int[] nums) {
+            int res = nums[0]
+        }
+
+    }
+
+
+    public static void main(String[] Args) {
+        Solution solution = new Solution();
+
+        int i = solution.maxProduct(new int[]{2,3,-2,4});
+        System.out.println(i);
+
+    }
+}
