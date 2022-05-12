25817701d5cbf990e512bc980e40eec8a7d576f1
frZheng <fr_zheng@163.com>
2022-05-12 16:07:31 +0800

    commit

--- top100/lruCache.java
+++ top100/lruCache.java
@@ -8,27 +8,93 @@ import list.ListNode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class  lruCache {
     private static class LRUCache {
 
-        int capacity;
-        HashMap<Integer, Integer> cache;
+        class DLinkedNode {
+            int key;
+            int value;
+            DLinkedNode prev;
+            DLinkedNode next;
+            public DLinkedNode() {}
+            public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
+        }
+
+        private Map<Integer, DLinkedNode> cache = new HashMap<>();
+        private int size;
+        private int capacity;
+        private DLinkedNode head, tail;
         public LRUCache(int capacity) {
+            this.size = 0;
             this.capacity = capacity;
-            cache = new HashMap<>();
+            // 使用伪头部和伪尾部节点
+            head = new DLinkedNode();
+            tail = new DLinkedNode();
+            head.next = tail;
+            tail.prev = head;
         }
 
         public int get(int key) {
-            return cache.getOrDefault(key,-1);
+            DLinkedNode node = cache.get(key);
+            if (node == null) {
+                return -1;
+            }
+            // 如果 key 存在，先通过哈希表定位，再移到头部
+            moveToHead(node);
+            return node.value;
         }
 
         public void put(int key, int value) {
-            if (cache.containsKey(key))
-                cache.put(key,value);
-
+            DLinkedNode node = cache.get(key);
+            if (node == null) {
+                // 如果 key 不存在，创建一个新的节点
+                DLinkedNode newNode = new DLinkedNode(key, value);
+                // 添加进哈希表
+                cache.put(key, newNode);
+                // 添加至双向链表的头部
+                addToHead(newNode);
+                ++size;
+                if (size > capacity) {
+                    // 如果超出容量，删除双向链表的尾部节点
+                    DLinkedNode tail = removeTail();
+                    // 删除哈希表中对应的项
+                    cache.remove(tail.key);
+                    --size;
+                }
+            }
+            else {
+                // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+                node.value = value;
+                moveToHead(node);
+            }
         }
+
+        private void addToHead(DLinkedNode node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        private void removeNode(DLinkedNode node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        private void moveToHead(DLinkedNode node) {
+            removeNode(node);
+            addToHead(node);
+        }
+
+        private DLinkedNode removeTail() {
+            DLinkedNode res = tail.prev;
+            removeNode(res);
+            return res;
+        }
+
     }
 
 
@@ -40,13 +106,13 @@ public class  lruCache {
         LRUCache cache = new LRUCache(2);
         cache.put(1,1);
         cache.put(2,2);
-        cache.get(1);
+        System.out.println(cache.get(1));
         cache.put(3,3);
-        cache.get(2);
+        System.out.println(cache.get(2));
         cache.put(4,4);
-        cache.get(1);
-        cache.get(3);
-        cache.get(4);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
 
     }
 
