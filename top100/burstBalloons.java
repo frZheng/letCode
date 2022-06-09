63ab3c3af1b65fe5cc59e637fc363c948c46a10a
frZheng <fr_zheng@163.com>
2022-06-09 13:00:00 +0800

    commit

---
+++ top100/burstBalloons.java
@@ -0,0 +1,35 @@
+package top100;
+
+//web https://leetcode.cn/problems/burst-balloons
+
+
+import list.ListNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class  burstBalloons {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public int maxCoins(int[] nums) {
+            
+        }
+
+    }
+
+
+
+    public static void main(String Args[]) {
+        Solution solution = new Solution();
+
+
+        int res = solution.maxCoins(new int[] {3,1,5,8});
+        System.out.println(res);
+    }
+
+}
