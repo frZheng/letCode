6b1359821d538840f2b29c102d234a8bf1d83423
frZheng <fr_zheng@163.com>
2022-06-14 18:00:00 +0800

    commit

---
+++ top100/decodeString.java
@@ -0,0 +1,30 @@
+package top100;
+
+//web https://leetcode.cn/problems/decode-string
+
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class  decodeString {
+    private static class Solution {
+
+        public Solution() {
+        }
+
+        public String decodeString(String s) {
+        }
+
+    }
+
+
+    public static void main(String[] Args) {
+        Solution solution = new Solution();
+
+        String lists = solution.decodeString("3[a]2[bc]");
+        System.out.println(lists);
+    }
+}
