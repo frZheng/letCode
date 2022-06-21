aaffa4cd8b0e0545cf38d89e772d59dc9d73dc3a
frZheng <fr_zheng@163.com>
2022-06-21 13:00:01 +0800

    commit

---
+++ top100/evaluateDivision.java
@@ -0,0 +1,35 @@
+package top100;
+
+//web https://leetcode.cn/problems/evaluate-division
+
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class  evaluateDivision {
+    private static class Solution {
+
+        public Solution() {
+        }
+        public double[] calcEquation(List<List<String>> equations,
+                                     double[] values,
+                                     List<List<String>> queries) {
+
+        }
+
+
+    }
+
+
+    public static void main(String[] Args) {
+        Solution solution = new Solution();
+        new ArrayList<equations>()
+        equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+
+        double[] lists = solution.calcEquation("3[a]2[bc]");
+        System.out.println(lists);
+    }
+}
