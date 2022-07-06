2ca1a5a42663f0379c6a43b2beba6d7174239a67
frZheng <fr_zheng@163.com>
2022-07-06 14:31:05 +0800

    commit

--- top100/findAllAnagramsInAString.java
+++ top100/findAllAnagramsInAString.java
@@ -15,7 +15,50 @@ public class   findAllAnagramsInAString {
         }
 
         public List<Integer> findAnagrams(String s, String p) {
-            return new ArrayList<>();
+            int sLen = s.length(), pLen = p.length();
+            List<Integer> ans = new ArrayList<Integer>();
+            if (sLen < pLen) {
+                return ans;
+            }
+
+            int[] count = new int[26];
+            for (int i = 0; i < pLen; ++i) {
+                ++count[s.charAt(i) - 'a'];
+                --count[p.charAt(i) - 'a'];
+            }
+
+            int differ = 0;
+            for (int j = 0; j < 26; ++j) {
+                if (count[j] != 0) {
+                    ++differ;
+                }
+            }
+
+            if (differ == 0) {
+                ans.add(0);
+            }
+
+            for (int i = 0; i < sLen - pLen; ++i) {
+                if (count[s.charAt(i) - 'a'] == 1) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                    --differ;
+                } else if (count[s.charAt(i) - 'a'] == 0) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                    ++differ;
+                }
+                --count[s.charAt(i) - 'a'];
+
+                if (count[s.charAt(i + pLen) - 'a'] == -1) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                    --differ;
+                } else if (count[s.charAt(i + pLen) - 'a'] == 0) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                    ++differ;
+                }
+                ++count[s.charAt(i + pLen) - 'a'];
+
+                if (differ == 0) {
+                    ans.add(i + 1);
+                }
+            }
+
+            return ans;
         }
 
     }
