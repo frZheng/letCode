0f6a1107d64b443af7b1d57c69551e89b1e15fe8
frZheng <fr_zheng@163.com>
2022-07-09 10:36:58 +0800

    commit

--- top100/subarraySumEqualsK.java
+++ top100/subarraySumEqualsK.java
@@ -3,11 +3,7 @@ package top100;
 //web https://leetcode.cn/problems/subarray-sum-equals-k
 
 
-import java.util.ArrayList;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class   subarraySumEqualsK {
     private static class Solution {
@@ -16,44 +12,29 @@ public class   subarraySumEqualsK {
         }
 
 
-
         public int subarraySum(int[] nums, int k) {
-            int size = nums.length;
-            int curSum = 0;
-            int i =0;
-            int sumNum = 0;
-            int res = 0;
-            while (i < size){
-                if (curSum + nums[i] < k){
-                    curSum += nums[i];
-                    sumNum++;
+            int count = 0, pre = 0;
+            HashMap< Integer, Integer > mp = new HashMap < > ();
+            mp.put(0, 1);
+            for (int i = 0; i < nums.length; i++) {
+                pre += nums[i];
+                if (mp.containsKey(pre - k)) {
+                    count += mp.get(pre - k);
                 }
-                else if (curSum + nums[i] == k){
-                    res++;
-                    curSum -= nums[i-sumNum];
-                    curSum += nums[i];
-                    sumNum--;
-                }
-                else {
-                    while (i < size && curSum < k) {
-                        curSum += nums[i];
-                        sumNum++;
-                        i++;
-                    }
-                }
-                i++;
+                mp.put(pre, mp.getOrDefault(pre, 0) + 1);
             }
-            return res;
+            return count;
         }
-
 
     }
 
 
     public static void main(String[] Args) {
         Solution solution = new Solution();
-        int[] nums={1,1,1};
-        int k = 1;
+//        int[] nums={1,2,1,2,1};
+//        int k = 3;
+        int[] nums={-1,-1,1};
+        int k = 0;
         int s = solution.subarraySum(nums,k);
         System.out.println(s);
 
