892c0618fc4ee4792e1b2dfd726812d07df0fd50
frZheng <fr_zheng@163.com>
2022-07-27 08:48:01 +0800

    commit

--- sql/FocusOn/biggestWindowBetweenVisits.sql
+++ sql/FocusOn/biggestWindowBetweenVisits.sql
@@ -10,4 +10,22 @@ insert into UserVisits values (2,'2020-12-9');
 insert into UserVisits values (3,'2020-11-11');
 select * from UserVisits;
 
-select user_id,diff(max(visit_date),min(visit_date)) biggest_window from UserVisits group by user_id;
+select user_id,datediff(max(visit_date),min(visit_date)) biggest_window from UserVisits group by user_id;
+
+
+select
+    user_id,max(days) biggest_window
+from
+    (
+        select
+            -- 这里是从visit_date里取1.[2020-10-5,2020-12-9] ->2.[2020-10-5,2020-12-9,2021-01-01]
+            -- datediff 的1个2进行计算1的2020-10-5 和 2020-12-9, 2020-12-9和2021-01-01
+            user_id, datediff(lead(visit_date,1,'2021-01-01')
+                over(partition by user_id order by visit_date),visit_date ) days
+        from
+            UserVisits
+    ) s1
+group by
+    user_id
+order by user_id
+
