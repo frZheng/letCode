cde771cbcafd90c4ee9ee982c379fac217e07c51
frZheng <fr_zheng@163.com>
2022-08-22 18:00:01 +0800

    commit

--- Main.java
+++ Main.java
@@ -1,9 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
+    static Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('A', 1);
+        put('B', 5);
+        put('C', 10);
+        put('D', 50);
+        put('E', 100);
+        put('F', 500);
+        put('G', 1000);
+    }};
+
+    public static int romanToInt(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] s = sc.nextLine().split(" ");
-        System.out.println(Integer.parseInt(s[0]) + Integer.parseInt(s[1]));
+        String next = sc.nextLine();
+        int n = Integer.parseInt(next);
+
+        StringBuilder data = new StringBuilder();
+        next = sc.nextLine();
+        String[] s = next.split(" ");
+        for (int i = 0; i < n; ++i) {
+            data.append(s[i]);
+        }
+
+        int radius = romanToInt(data.toString());
+        System.out.println(radius);
     }
 }
