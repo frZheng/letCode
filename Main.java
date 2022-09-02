3d48152335c072373f561473406c5b1ba877b7f6
frZheng <fr_zheng@163.com>
2022-09-02 18:00:00 +0800

    commit

--- Main.java
+++ Main.java
@@ -2,29 +2,7 @@ import java.util.*;
 
 
 public class Main {
-    static Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
-        put('A', 1);
-        put('B', 5);
-        put('C', 10);
-        put('D', 50);
-        put('E', 100);
-        put('F', 500);
-        put('G', 1000);
-    }};
 
-    public static int romanToInt(String s) {
-        int ans = 0;
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            int value = symbolValues.get(s.charAt(i));
-            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
-                ans -= value;
-            } else {
-                ans += value;
-            }
-        }
-        return ans;
-    }
 
 
     public static void main(String[] args) {
