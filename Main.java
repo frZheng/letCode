5b805789540c89a4adaab85cbcab6c8ae19f9286
frZheng <fr_zheng@163.com>
2022-09-16 13:00:02 +0800

    commit

--- Main.java
+++ Main.java
@@ -4,27 +4,21 @@ import java.util.*;
 public class Main {
 
 
-    static boolean judge(String s){
-        int len = s.length();
-        boolean[] visit = new boolean[len];
-        s.su
-        int first=0;
-        for(int i =0;i<len)
-    }
+//    static boolean judge(String s){
+//        int len = s.length();
+//        boolean[] visit = new boolean[len];
+//        s.su
+//        int first=0;
+//        for(int i =0;i<len)
+//    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Integer n = Integer.valueOf(scanner.nextLine());
-        for (int i =0;i < n; ++i){
-            boolean judge = judge(scanner.nextLine());
-            if(judge) System.out.println("No");
-            else System.out.println("Yes");
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        Integer n = Integer.valueOf(scanner.nextLine());
+//        for (int i =0;i < n; ++i){
+//            boolean judge = judge(scanner.nextLine());
+//            if(judge) System.out.println("No");
+//            else System.out.println("Yes");
+//        }
     }
 }
 
-1
-baab
-3
-abcdcba
-aab
-baab
